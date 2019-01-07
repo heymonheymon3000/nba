b8e1256b94b772c84a6875e2dbf04a8808dbeee2
heymonheymon3000@yahoo.com <terry.parrish@emory.edu>
2019-01-07 15:35:22 -0500

    adding game detail

--- app/src/main/java/nanodegree/android/nba/ui/gameDetail/GameDetailFragment.java
+++ app/src/main/java/nanodegree/android/nba/ui/gameDetail/GameDetailFragment.java
@@ -1,20 +1,27 @@
 package nanodegree.android.nba.ui.gameDetail;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
+import nanodegree.android.nba.NBAApplication;
 import nanodegree.android.nba.R;
 import nanodegree.android.nba.persistence.entity.GameAgg;
+import nanodegree.android.nba.utils.DisplayMetricUtils;
+import nanodegree.android.nba.utils.TeamInfo;
 
 public class GameDetailFragment extends Fragment {
     public final static String GAME_AGG_KEY = "gameAgg";
     private GameAgg gameAgg;
+    private ImageView awayImageView;
+    private ImageView homeImageView;
+    private Picasso picassoInstance;
 
     public GameDetailFragment() {}
 
@@ -33,12 +40,46 @@ public class GameDetailFragment extends Fragment {
         if (getArguments() != null) {
             gameAgg = getArguments().getParcelable(GAME_AGG_KEY);
         }
+
+        picassoInstance =
+                new Picasso.Builder(getContext())
+                        .loggingEnabled(true)
+                        .build();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_game_detail, container, false);
+
+        Toolbar mToolbar = (Toolbar) rootView.findViewById(R.id.toolbar);
+        mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().finish();
+            }
+        });
+
+        TeamInfo awayTeamInfo = NBAApplication.teamInfoHashMap.get(gameAgg.getAwayAlias());
+        TeamInfo homeTeamInfo = NBAApplication.teamInfoHashMap.get(gameAgg.getHomeAlias());
+
+        awayImageView = rootView.findViewById(R.id.away_team_logo_image_view);
+        homeImageView = rootView.findViewById(R.id.home_team_logo_image_view);
+
+        picassoInstance
+                .load(awayTeamInfo.getLogo())
+                .resize(DisplayMetricUtils.convertDpToPixel(100),
+                        DisplayMetricUtils.convertDpToPixel(100))
+                .centerCrop()
+                .into(awayImageView);
+
+        picassoInstance
+                .load(homeTeamInfo.getLogo())
+                .resize(DisplayMetricUtils.convertDpToPixel(100),
+                        DisplayMetricUtils.convertDpToPixel(100))
+                .centerCrop()
+                .into(homeImageView);
 
 
         return rootView;
