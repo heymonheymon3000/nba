00f2a4ec8733562464fd501ab5b66b38fafa0248
heymonheymon3000@yahoo.com <terry.parrish@emory.edu>
2019-01-08 01:19:12 -0500

    adding game detail

--- app/src/main/java/nanodegree/android/nba/ui/gameDetail/GameDetailFragment.java
+++ app/src/main/java/nanodegree/android/nba/ui/gameDetail/GameDetailFragment.java
@@ -7,9 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import org.w3c.dom.Text;
+
+import nanodegree.android.nba.R;
 import nanodegree.android.nba.NBAApplication;
 import nanodegree.android.nba.R;
 import nanodegree.android.nba.persistence.entity.GameAgg;
@@ -22,6 +26,11 @@ public class GameDetailFragment extends Fragment {
     private ImageView awayImageView;
     private ImageView homeImageView;
     private Picasso picassoInstance;
+    private TextView awayTeamScore;
+    private TextView homeTeamScore;
+    private TextView mGameStatusTextView;
+    private TextView mHomeTeamName;
+    private TextView mAwayTeamName;
 
     public GameDetailFragment() {}
 
@@ -66,22 +75,43 @@ public class GameDetailFragment extends Fragment {
 
         awayImageView = rootView.findViewById(R.id.away_team_logo_image_view);
         homeImageView = rootView.findViewById(R.id.home_team_logo_image_view);
+        mGameStatusTextView = rootView.findViewById(R.id.game_status_text_view);
+        mHomeTeamName = rootView.findViewById(R.id.home_team_logo_text_view);
+        mAwayTeamName = rootView.findViewById(R.id.away_team_logo_text_view);
 
         picassoInstance
                 .load(awayTeamInfo.getLogo())
-                .resize(DisplayMetricUtils.convertDpToPixel(100),
-                        DisplayMetricUtils.convertDpToPixel(100))
+                .resize(DisplayMetricUtils.convertDpToPixel(60),
+                        DisplayMetricUtils.convertDpToPixel(60))
                 .centerCrop()
                 .into(awayImageView);
 
         picassoInstance
                 .load(homeTeamInfo.getLogo())
-                .resize(DisplayMetricUtils.convertDpToPixel(100),
-                        DisplayMetricUtils.convertDpToPixel(100))
+                .resize(DisplayMetricUtils.convertDpToPixel(60),
+                        DisplayMetricUtils.convertDpToPixel(60))
                 .centerCrop()
                 .into(homeImageView);
 
+        if(gameAgg.getAwayPoints() != null && gameAgg.getHomePoints() != null) {
+            awayTeamScore = rootView.findViewById(R.id.away_team_score_text_view);
+            awayTeamScore.setText(gameAgg.getAwayPoints());
+
+            homeTeamScore = rootView.findViewById(R.id.home_team_score_text_view);
+            homeTeamScore.setText(gameAgg.getHomePoints());
+        }
+
+        mGameStatusTextView.setText(gameAgg.getTimeOnClock());
+
+        mHomeTeamName.setText(getShortName(gameAgg.getHomeName()));
+        mAwayTeamName.setText(getShortName(gameAgg.getAwayName()));
 
         return rootView;
+    }
+
+    private String getShortName(String name) {
+        String[] result = name.split(" ");
+        return result[result.length-1];
+
     }
 }
